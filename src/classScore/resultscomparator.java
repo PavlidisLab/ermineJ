cdb1ee185a609cb0550806fa06229ff9b133e79b
pavlidis <>
2003-02-18 23:55:10 +0000

    Moved files to shallower directory structure

---
+++ src/classScore/resultscomparator.java
@@ -0,0 +1,14 @@
+package scores.class_score;
+import java.util.*;
+
+class resultscomparator implements Comparator {
+  public int compare(Object o1, Object o2) {
+    classresult s1 = (classresult)o1;
+    classresult s2 = (classresult)o2;
+    return s1.compareTo(s2);
+  } 
+
+  public boolean equals(Object o) {
+    return compare(this, o)==0;
+  }
+}
