e8b068cc2bd0aaf5aeb87dc0e4dfd0a516418cac
wbraynen <>
2004-06-12 18:54:46 +0000

    Just added some comments, that's all.

--- src/classScore/SortFilterModel.java
+++ src/classScore/SortFilterModel.java
@@ -15,53 +15,59 @@ import javax.swing.table.*;
 
 public class SortFilterModel
     extends AbstractTableModel {
-   private TableModel model;
-   private int sortColumn;
-   private Row[] rows;
+    private TableModel model;
+    private int sortColumn;
+    private Row[] rows;
 
-   public SortFilterModel(TableModel m) {
-      model = m;
-      rows = new Row[model.getRowCount()];
-      for (int i = 0; i < rows.length; i++) {
-         rows[i] = new Row();
-         rows[i].index = i;
-      }
-   }
+    public SortFilterModel(TableModel m) {
+	model = m;
+	rows = new Row[model.getRowCount()];
+      
+	// Initially, underlying row order is the same as displayed row order
+	for (int i = 0; i < rows.length; i++) {
+	    rows[i] = new Row();
+	    rows[i].index = i;
+	}
+    }
 
-   public void sort(int c) {
-      sortColumn = c;
-      Arrays.sort(rows);
-//     fireTableDataChanged();
-   }
+    public void sort(int c) {
+	sortColumn = c;
+	Arrays.sort(rows);
+	//     fireTableDataChanged();
+    }
 
-   public Object getValueAt(int r, int c) {
-      return model.getValueAt(rows[r].index, c);
-   }
+    /** 
+     * Translate from the row order that is displayed
+     * to the underlying row order.
+     */
+    public Object getValueAt(int r, int c) {
+	return model.getValueAt(rows[r].index, c);
+    }
 
-   public int getRowCount() {
-      return model.getRowCount();
-   }
+    public int getRowCount() {
+	return model.getRowCount();
+    }
 
-   public int getColumnCount() {
-      return model.getColumnCount();
-   }
+    public int getColumnCount() {
+	return model.getColumnCount();
+    }
 
-   public String getColumnName(int c) {
-      return model.getColumnName(c);
-   }
+    public String getColumnName(int c) {
+	return model.getColumnName(c);
+    }
 
    private class Row
        implements Comparable {
-      public int index;
-      public int compareTo(Object other) {
-         Row otherRow = (Row) other;
-         Object a = model.getValueAt(index, sortColumn);
-         Object b = model.getValueAt(otherRow.index, sortColumn);
-         if (a instanceof Comparable) {
-            return ( (Comparable) a).compareTo(b);
-         } else {
-            return a.toString().compareTo(b.toString());
-         }
-      }
+       public int index;
+       public int compareTo(Object other) {
+	   Row otherRow = (Row) other;
+	   Object a = model.getValueAt(index, sortColumn);
+	   Object b = model.getValueAt(otherRow.index, sortColumn);
+	   if (a instanceof Comparable) {
+	       return ( (Comparable) a).compareTo(b);
+	   } else {
+	       return a.toString().compareTo(b.toString());
+	   }
+       }
    }
- }
+}
