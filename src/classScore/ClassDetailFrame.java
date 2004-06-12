761aed6348aa277a06befd5d31dc800adff2eef9
wbraynen <>
2004-06-12 18:53:20 +0000

    The matrix can now be displayed here, but from a dummy file.  The matrix display does not actually paint itself as was originally intende.  Instead, its cells are painted by the cell renderer (MatrixDisplayCellRenderer class in this file).  This way the sorting is already implemented by SortFilterModel and there are even mouseover tooltips which display the numeric value of a matrix display color cell (when you place the mouse pointer over them).  The next step is to make the data automatically extractable from the master file (grep-ing the lines containing microarray data for the genes in the given GO class).

--- src/classScore/ClassDetailFrame.java
+++ src/classScore/ClassDetailFrame.java
@@ -7,6 +7,7 @@ import javax.swing.table.*;
 import java.util.Map;
 import java.util.ArrayList;
 import java.text.NumberFormat;
+import baseCode.gui.JMatrixDisplay;
 
 /**
  * <p>Title: </p>
@@ -16,133 +17,178 @@ import java.text.NumberFormat;
  * @author not attributable
  * @version 1.0
  * @todo show how many rows there are in the table (I think this is done classPvalRun, line 906)
-* @todo add a graphical display of the data
+ * @todo add a graphical display of the data
  */
 
 public class ClassDetailFrame
     extends JFrame {
-   JScrollPane jScrollPane1 = new JScrollPane();
-   JTable jTable1 = new JTable();
+    JScrollPane jScrollPane1 = new JScrollPane();
+    JTable jTable1 = new JTable();
 
-   public ClassDetailFrame( 
-     ArrayList values, 
-     Map pvals, 
-     Map classToProbe, 
-     String id, 
-     NumberFormat nf,
-     GeneDataReader geneData ) 
-   {
-      try {
-         jbInit();
-         createDetailsTable( values, pvals, classToProbe, id, nf, geneData );
-      }
-      catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+    public ClassDetailFrame( 
+			    ArrayList values, 
+			    Map pvals, 
+			    Map classToProbe, 
+			    String id, 
+			    NumberFormat nf,
+			    GeneDataReader geneData ) 
+    {
+	try {
+	    jbInit();
+	    createDetailsTable( values, pvals, classToProbe, id, nf, geneData );
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
 
-   private void jbInit() throws Exception {
-      this.setSize(500, 460);
-      this.setLocation(200, 100);
-      this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    private void jbInit() throws Exception {
+	this.setSize(500, 460);
+	this.setLocation(200, 100);
+	this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
       
-      jTable1.setGridColor(Color.lightGray);      
+	jTable1.setGridColor(Color.lightGray);
 
-      // add a viewport with a table inside it
-      this.getContentPane().add(jScrollPane1, BorderLayout.CENTER);
-      jScrollPane1.getViewport().add(jTable1, null);
-   }
+	// add a viewport with a table inside it
+	this.getContentPane().add(jScrollPane1, BorderLayout.CENTER);
+	jScrollPane1.getViewport().add(jTable1, null);
+    }
 
-   private void createDetailsTable( 
-        ArrayList values, 
-        Map pvals, 
-        Map classToProbe, 
-        String id, 
-        NumberFormat nf,
-        GeneDataReader geneData ) 
-   {
-
-      DetailsTableModel m = new DetailsTableModel( values, pvals, classToProbe, id, nf, geneData );
+    private void createDetailsTable( 
+				    ArrayList values, 
+				    Map pvals, 
+				    Map classToProbe, 
+				    String id, 
+				    NumberFormat nf,
+				    GeneDataReader geneData ) 
+    {
       
-      SortFilterModel sorter = new SortFilterModel(m);
-      jTable1.setModel(sorter);
-      jTable1.getTableHeader().addMouseListener(new MouseAdapter() {
-         public void mouseClicked(MouseEvent event) {
-            int tableColumn = jTable1.columnAtPoint(event.getPoint());
-            int modelColumn = jTable1.convertColumnIndexToModel(tableColumn);
-            ( (SortFilterModel) jTable1.getModel()).sort(modelColumn);
-         }
-      });
-
-      jTable1.getColumnModel().getColumn(1).setPreferredWidth(75);
-      jTable1.getColumnModel().getColumn(2).setPreferredWidth(125);
-      jTable1.getColumnModel().getColumn(3).setPreferredWidth(300);
+	//
+	// Create a matrix display
+	//
+	JMatrixDisplay matrixDisplay = null;
+	String filename = "C:\\GO_0005853_partial.txt"; //GO_0005853.txt";
+	try {
+	    matrixDisplay = new JMatrixDisplay( filename );
+	} catch (java.io.IOException e) {
+	    System.err.println( "IOException: wrong filename for JMatrixDisplay");
+	}
+	matrixDisplay.setStandardizedEnabled( true );
       
-//      // Install the custom renderer on the first visible column
-//      int vColIndex = 0;
-//      TableColumn col = jTable1.getColumnModel().getColumn(vColIndex);
-//      col.setCellRenderer( new MatrixDisplayCellRenderer() );      
-   }
+	//
+	// Create the rest of the table
+	//
+	DetailsTableModel m = new DetailsTableModel( matrixDisplay, values, pvals, classToProbe, id, nf, geneData );
+	SortFilterModel sorter = new SortFilterModel( m );
+	jTable1.setModel( sorter );
+
+	jTable1.getTableHeader().addMouseListener(new MouseAdapter() {
+		public void mouseClicked( MouseEvent event ) {
+		    int tableColumn = jTable1.columnAtPoint( event.getPoint() );
+		    int modelColumn = jTable1.convertColumnIndexToModel( tableColumn );
+		    ( (SortFilterModel) jTable1.getModel() ).sort( modelColumn );
+		}
+	    });
+      
+	// Make the columns in the matrix display not too wide (cell-size)
+	// and set a custom cell renderer
+	MatrixDisplayCellRenderer cellRenderer = new MatrixDisplayCellRenderer( matrixDisplay );
+	int matrixColumnCount = matrixDisplay.getColumnCount();
+	for (int i = 0;  i < matrixColumnCount;  i++)
+	    {
+		jTable1.getColumnModel().getColumn( i ).setPreferredWidth( 10 );
+		jTable1.getColumnModel().getColumn( i ).setMaxWidth( 10 );
+		jTable1.getColumnModel().getColumn( i ).setMinWidth( 10 );
+		jTable1.getColumnModel().getColumn( i ).setResizable( false );
+		TableColumn col = jTable1.getColumnModel().getColumn( i );
+		col.setCellRenderer( cellRenderer );
+	    }
+
+	// The columns containing text or values (not matrix display) should be a bit wider
+	jTable1.getColumnModel().getColumn( matrixColumnCount + 1 ).setPreferredWidth(75);
+	jTable1.getColumnModel().getColumn( matrixColumnCount + 2 ).setPreferredWidth(125);
+	jTable1.getColumnModel().getColumn( matrixColumnCount + 3 ).setPreferredWidth(300);
+      
+    }
 }
 
 class DetailsTableModel extends AbstractTableModel {
 
-   private ArrayList m_values;
-   private Map m_pvals;
-   private Map m_classToProbe;
-   private String m_id;
-   private NumberFormat m_nf;
-   private GeneDataReader m_geneData;
-   private String[] m_columnNames = { "Probe", "P value", "Name", "Description" };
+    private JMatrixDisplay m_matrixDisplay;
+    private ArrayList m_values;
+    private Map m_pvals;
+    private Map m_classToProbe;
+    private String m_id;
+    private NumberFormat m_nf;
+    private GeneDataReader m_geneData;
+    private String[] m_columnNames = { "Probe", "P value", "Name", "Description" };
 
-   /** constructor */
-   public DetailsTableModel( 
-     ArrayList values, 
-     Map pvals, 
-     Map classToProbe, 
-     String id, 
-     NumberFormat nf,
-     GeneDataReader geneData ) {
+    /** constructor */
+    public DetailsTableModel( 
+			     JMatrixDisplay matrixDisplay,
+			     ArrayList values,
+			     Map pvals,
+			     Map classToProbe,
+			     String id,
+			     NumberFormat nf,
+			     GeneDataReader geneData ) {
       
-      m_values = values;
-      m_pvals = pvals;
-      m_classToProbe = classToProbe;
-      m_id = id;
-      m_nf = nf;
-      m_geneData = geneData;
-   }
+	m_matrixDisplay = matrixDisplay;
+	m_values = values;
+	m_pvals = pvals;
+	m_classToProbe = classToProbe;
+	m_id = id;
+	m_nf = nf;
+	m_geneData = geneData;
+    }
    
-   public String getColumnName(int i) {
-      return m_columnNames[i];
-   }
-
-   public int getRowCount() {
-      return m_values.size();
-   }
-
-   public int getColumnCount() {
-      return 4;
-   }
-
-   public Object getValueAt(int i, int j) {
+    public String getColumnName( int column ) {
       
-      switch (j) {
-         case 0:
-            return (String) ( (ArrayList) m_classToProbe.get(m_id)).get(i);
-         case 1:
-            return new Double(m_nf.format(m_pvals.get( (String) ( (ArrayList)
-                m_classToProbe.get(m_id)).get(i))));
-         case 2:
-            return m_geneData.getProbeGeneName( (String) ( (ArrayList)
-                m_classToProbe.get(m_id)).get(i));
-         case 3:
-            return m_geneData.getProbeDescription( (String) ( (ArrayList)
-                m_classToProbe.get(m_id)).get(i));
-         default:
-            return "";
-      }
-   }
+	int offset = m_matrixDisplay.getColumnCount(); // matrix display ends
+      
+	if (column < offset)
+	    return ""; //m_matrixDisplay.getColumnName( column );
+	else
+	    return m_columnNames[ column - offset ];
+    }
+
+    public int getRowCount() {
+	return m_values.size();
+    }
+
+    public int getColumnCount() {
+	return m_columnNames.length + m_matrixDisplay.getColumnCount();
+    }
+
+    public Object getValueAt( int row, int column ) {
+
+	int offset = m_matrixDisplay.getColumnCount(); // matrix display ends
+      
+	if (column < offset)
+	    return new Point( row, column ); // coords into JMatrixDisplay
+	else
+	    column -= offset;
+
+	switch( column ) // after it's been offset
+	    {
+	    case 0:
+		return (String) ( (ArrayList) m_classToProbe.get(m_id)).get(row);
+	    case 1:
+		return new Double(m_nf.format(m_pvals.get( (String) ( (ArrayList)
+								      m_classToProbe.get(m_id)).get(row))));
+	    case 2:
+		return m_geneData.getProbeGeneName( (String) ( (ArrayList)
+							       m_classToProbe.get(m_id)).get(row));
+	    case 3:
+		return m_geneData.getProbeDescription( (String) ( (ArrayList)
+								  m_classToProbe.get(m_id)).get(row));
+	    default:
+		return "";
+	    }
+      
+    } // end getValueAt
+   
 } // end class DetailsTableModel
 
 
@@ -150,41 +196,55 @@ class DetailsTableModel extends AbstractTableModel {
 // cell must be displayed.
 class MatrixDisplayCellRenderer extends JLabel implements TableCellRenderer {
    
-     public MatrixDisplayCellRenderer() {
-        
-        setOpaque( true );
-     }
+    JMatrixDisplay m_matrixDisplay;
    
-     // This method is called each time a cell in a column
-     // using this renderer needs to be rendered.
-     public Component getTableCellRendererComponent(JTable table, Object value,
-             boolean isSelected, boolean hasFocus, int rowIndex, int vColIndex) {
-         // 'value' is value contained in the cell located at
-         // (rowIndex, vColIndex)
+    public MatrixDisplayCellRenderer( JMatrixDisplay matrixDisplay ) {
 
-         if (isSelected) {
-             // cell (and perhaps other cells) are selected
-         }
+	m_matrixDisplay = matrixDisplay;
+	setOpaque( true );
+    }
+   
+    // This method is called each time a cell in a column
+    // using this renderer needs to be rendered.
+    public Component getTableCellRendererComponent(
+						   JTable table, 
+						   Object tableCellValue,
+						   boolean isSelected, 
+						   boolean hasFocus, 
+						   int displayedRow, 
+						   int displayedColumn) 
+    {
+	// 'value' is value contained in the cell located at
+	// (rowIndex, vColIndex)
 
-         if (hasFocus) {
-             // this cell is the anchor and the table has the focus
-         }
+	if (isSelected) {
+	    // cell (and perhaps other cells) are selected
+	}
 
-         // Configure the component with the specified value
-         //setText(value.toString());
+	if (hasFocus) {
+	    // this cell is the anchor and the table has the focus
+	}
 
-         // Set tool tip if desired
-         setToolTipText((String)value);
-         setBackground( Color.red );
+	Point coords = (Point) tableCellValue;
+	int row    = coords.x;
+	int column = coords.y;
+	double matrixValue = m_matrixDisplay.getValue( row, column );
+	Color matrixColor = m_matrixDisplay.getColor( row, column );
+            
+	// Configure the component with the specified value
+	setBackground( matrixColor );
 
-         // Since the renderer is a component, return itself
-         return this;
-     }
+	// Set tool tip if desired
+	setToolTipText( "" + matrixValue );
 
-     // The following methods override the defaults for performance reasons
-     public void validate() {}
-     public void revalidate() {}
-     protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {}
-     public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {}
- }
+	// Since the renderer is a component, return itself
+	return this;
+    }
 
+    // The following methods override the defaults for performance reasons
+    //public void validate() {}
+    //public void revalidate() {}
+    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {}
+    public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {}
+
+} // end class MatrixDisplayCellRenderer
