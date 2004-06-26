6568138759b5bc166cd3b77838de5b45a9781046
wbraynen <>
2004-06-26 05:19:36 +0000

    The save options are now checkboxes instead of combo boxes.  Also, they should by default now reflect the state of the main window; for example, if the matrix display is not normalized, then the "normalize" option in the "save image" dialog will be unselected by default.

--- src/classScore/gui/geneSet/JImageFileChooser.java
+++ src/classScore/gui/geneSet/JImageFileChooser.java
@@ -5,7 +5,7 @@ package classScore.gui.geneSet;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
-import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
@@ -22,7 +22,7 @@ public class JImageFileChooser
    JDetailsFileChooserOptions m_options;
 
    /** Creates a new instance of JDetailsFileChooser */
-   public JImageFileChooser() {
+   public JImageFileChooser( boolean includeLabels, boolean normalize ) {
 
       super();
 
@@ -32,7 +32,7 @@ public class JImageFileChooser
       super.setAcceptAllFileFilterUsed( false );
 
       // Create other save options (e.g. include row and column labels in image)
-      m_options = new JDetailsFileChooserOptions();
+      m_options = new JDetailsFileChooserOptions( includeLabels, normalize );
       super.setAccessory( m_options );
    }
 
@@ -43,64 +43,58 @@ public class JImageFileChooser
    public boolean includeLabels() {
       return m_options.includeLabels();
    }
+   
+   /**
+    * The accessory component for the file chooser.<p>
+    *
+    * Lets the user change save options and might also in the future include
+    * a preview of what the matrix image will look like with the chosen options.
+    */
+   class JDetailsFileChooserOptions
+       extends JComponent {
+
+      JCheckBox m_includeLabels = new JCheckBox( "Include row and column labels" );
+      JCheckBox m_normalize = new JCheckBox( "Normalize" );
+
+      GridLayout gridLayout1 = new GridLayout( 12, 2 );
+      JLabel m_titleLabel = new JLabel();
+      JLabel m_spacerLabel = new JLabel();
+
+      public JDetailsFileChooserOptions( boolean includeLabels, boolean normalize ) throws HeadlessException {
+         try {
+            m_includeLabels.setSelected( includeLabels );
+            m_normalize.setSelected( normalize );
+            jbInit();
+         }
+         catch ( Exception e ) {
+            e.printStackTrace();
+         }
+      }
+
+      private void jbInit() throws Exception {
+         setLayout( gridLayout1 );
+         this.setMaximumSize( new Dimension( 264, 63 ) );
+         this.setMinimumSize( new Dimension( 264, 63 ) );
+         m_titleLabel.setHorizontalAlignment( SwingConstants.CENTER );
+         m_titleLabel.setText( "Save Options:" );
+         m_spacerLabel.setText( "" );
+         gridLayout1.setColumns( 2 );
+         gridLayout1.setHgap( 5 );
+         gridLayout1.setRows( 9 );
+         gridLayout1.setVgap( 5 );
+         this.add( m_spacerLabel, null );
+         this.add( m_titleLabel, null );
+         this.add( m_includeLabels, null );
+         this.add( m_normalize, null );
+      }
+
+      public boolean normalized() {
+         return m_normalize.isSelected();
+      }
+
+      public boolean includeLabels() {
+         return m_includeLabels.isSelected();
+      }
+   } // end private class JImageFileChooserOptions   
+
 } // end class JDetailsFileChooser
-
-/**
- * The accessory component for the file chooser.<p>
- *
- * Lets the user change save options and might also in the future include
- * a preview of what the matrix image will look like with the chosen options.
- */
-class JDetailsFileChooserOptions
-    extends JComponent {
-
-   String[] m_includeLabelsOptions = {
-       "Include row and column labels",
-       "Do not include labels"
-   };
-   JComboBox m_includeLabelsComboBox = new JComboBox( m_includeLabelsOptions );
-
-   String[] m_normalizeOptions = {
-       "Normalize to variance 1, mean 0",
-       "Do not normalize"
-   };
-   JComboBox m_normalizeComboBox = new JComboBox( m_normalizeOptions );
-
-   GridLayout gridLayout1 = new GridLayout( 12, 2 );
-   JLabel m_titleLabel = new JLabel();
-   JLabel m_spacerLabel = new JLabel();
-
-   public JDetailsFileChooserOptions() throws HeadlessException {
-      try {
-         jbInit();
-      }
-      catch ( Exception e ) {
-         e.printStackTrace();
-      }
-   }
-
-   private void jbInit() throws Exception {
-      setLayout( gridLayout1 );
-      this.setMaximumSize( new Dimension( 264, 63 ) );
-      this.setMinimumSize( new Dimension( 264, 63 ) );
-      m_titleLabel.setHorizontalAlignment( SwingConstants.CENTER );
-      m_titleLabel.setText( "Save Options:" );
-      m_spacerLabel.setText( "" );
-      gridLayout1.setColumns( 2 );
-      gridLayout1.setHgap( 5 );
-      gridLayout1.setRows( 9 );
-      gridLayout1.setVgap( 5 );
-      this.add( m_spacerLabel, null );
-      this.add( m_titleLabel, null );
-      this.add( m_includeLabelsComboBox, null );
-      this.add( m_normalizeComboBox, null );
-   }
-
-   public boolean normalized() {
-      return ( 0 == m_normalizeComboBox.getSelectedIndex() );
-   }
-
-   public boolean includeLabels() {
-      return ( 0 == m_includeLabelsComboBox.getSelectedIndex() );
-   }
-} // end JDetailsFileChooserOptions
