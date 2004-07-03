7808a8715cc476c037da1556182de3e4dbfe7950
pavlidis <>
2004-07-03 01:15:55 +0000

    helpframe added

---
+++ src/classScore/gui/HelpFrame.java
@@ -0,0 +1,22 @@
+package classScore.gui;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004 Columbia University
+ * @author pavlidis
+ * @version $Id$
+ */
+public class HelpFrame extends JFrame {
+
+   /**
+    * @param frame
+    */
+   public HelpFrame( GeneSetScoreFrame callingframe ) {
+      // TODO Auto-generated constructor stub
+   }
+
+}
