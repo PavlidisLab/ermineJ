01e66096517c392fd7f71da54765bc2980e46428
wbraynen <>
2004-07-07 15:47:55 +0000

    JavaHelp files.

---
+++ ermineJ-help/main.hs
@@ -0,0 +1,39 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "../dtd/helpset_2_0.dtd">
+
+<helpset version="1.0">
+
+  <!-- title -->
+  <title>ermineJ Help</title>
+
+  <!-- maps -->
+  <maps>
+     <homeID>top</homeID>
+     <mapref location="map.jhm"/>
+  </maps>
+
+  <!-- views -->
+  <view>
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>toc.xml</data>
+  </view>
+
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>index.xml</data>
+  </view>
+
+  <presentation>
+     <name>main</name>
+     <size width="400" height="400" />
+     <location x="200" y="200" />
+     <title>ermineJ Help</title>
+  </presentation>
+</helpset>
+
