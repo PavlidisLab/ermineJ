48c48576a9aea67d95ef4d5060c24b01e3265ed4
pavlidis <>
2004-07-22 13:07:50 +0000

    Added default preferences file. We may have to change this.

--- nsis-installer/files/bin/ermineJ.bat
+++ nsis-installer/files/bin/ermineJ.bat
@@ -1,2 +1,2 @@
-java -Xmx256m  -classpath ..\lib\ermineJ-help.jar;..\lib\baseCode.jar;..\lib\colt.jar;..\lib\commons-logging.jar;..\lib\jhelp.jar;..\lib\xercesImpl.jar;..\lib\ermineJ.jar -jar ..\lib\ermineJ.jar
+java -Xmx256m  -classpath ..\lib\ermineJ-help.jar;..\lib\baseCode.jar;..\lib\colt.jar;..\lib\commons-logging.jar;..\lib\jhelp.jar;..\lib\xercesImpl.jar;..\lib\ermineJ.jar  classScore/classScoreGUI
 
