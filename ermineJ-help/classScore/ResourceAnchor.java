236d425cf372743dc9bbafbfb47a8b0b2fc5b4bd
pavlidis <>
2005-05-06 18:08:02 +0000

    moving things around...

---
+++ ermineJ-help/classScore/ResourceAnchor.java
@@ -0,0 +1,13 @@
+package classScore;
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class ResourceAnchor {
+    public ResourceAnchor() {
+    }
+}
