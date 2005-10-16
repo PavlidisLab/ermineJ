c1adc387b1fc562bff04bb36fdada45d9d47761f
pavlidis <>
2005-10-16 17:20:21 +0000

    include stack trace after error

--- src/classScore/classScoreCMD.java
+++ src/classScore/classScoreCMD.java
@@ -84,7 +84,8 @@ public class classScoreCMD {
                 ResultsPrinter rp = new ResultsPrinter( saveFileName, result, goData, saveAllGenes );
                 rp.printResults( true );
             } catch ( Exception e ) {
-                statusMessenger.showStatus( "Error During analysis" + e );
+                statusMessenger.showStatus( "Error During analysis: \n" + e );
+                e.printStackTrace();
             }
         } else {
             try {
