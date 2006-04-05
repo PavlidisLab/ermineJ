03a66137843cceae75b21986978488928071ee49
ppavlidis <paul@chibi.ubc.ca>
2006-04-05 23:06:22 +0000

    fix main class path.

--- nsis-installer/files/bin/ermineJ.bat
+++ nsis-installer/files/bin/ermineJ.bat
@@ -1,2 +1,2 @@
-start javaw -Xmx256m  -classpath ..\lib\ermineJ-help.jar;..\lib\baseCode.jar;..\lib\colt.jar;..\lib\commons-logging.jar;..\lib\commons-configuration.jar;..\lib\commons-lang.jar;..\lib\opencsv.jar;..\lib\commons-collections.jar;..\lib\jhelp.jar;..\lib\xercesImpl.jar;..\lib\ermineJ.jar;..\lib\log4j.jar  ubic/ermineJ/classScoreGUI
+start javaw -Xmx256m  -classpath ..\lib\ermineJ-help.jar;..\lib\baseCode.jar;..\lib\colt.jar;..\lib\commons-logging.jar;..\lib\commons-configuration.jar;..\lib\commons-lang.jar;..\lib\opencsv.jar;..\lib\commons-collections.jar;..\lib\jhelp.jar;..\lib\xercesImpl.jar;..\lib\ermineJ.jar;..\lib\log4j.jar  ubic/erminej/classScoreGUI
 
