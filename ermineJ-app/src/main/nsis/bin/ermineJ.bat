b2c1766d8e1ea6e38b64721d859b881ec7d5e382
ppavlidis <paul@chibi.ubc.ca>
2011-08-06 03:12:30 +0000

    attempted fixes for missing jars from builds.

--- ermineJ-app/src/main/nsis/bin/ermineJ.bat
+++ ermineJ-app/src/main/nsis/bin/ermineJ.bat
@@ -1,2 +1,2 @@
-start javaw -Xmx1024m  -classpath ..\lib\ermineJ-help.jar;..\lib\baseCode.jar;..\lib\colt.jar;..\lib\commons-logging.jar;..\lib\commons-configuration.jar;..\lib\commons-lang.jar;..\lib\opencsv.jar;..\lib\commons-collections.jar;..\lib\jh.jar;..\lib\xercesImpl.jar;..\lib\ermineJ-app.jar;..\lib\log4j.jar  ubic/erminej/classScoreGUI
+start javaw -Xmx1024m  -classpath ..\lib\ermineJ-help.jar;..\lib\baseCode.jar;..\lib\colt.jar;..\lib\commons-logging.jar;..\lib\commons-configuration.jar;..\lib\commons-lang.jar;..\lib\jfreechart.jar;\lib\jfreecommon.jar;..\lib\commons-collections.jar;..\lib\jh.jar;..\lib\xercesImpl.jar;..\lib\ermineJ-app.jar;..\lib\log4j.jar ;..\lib\commons-math.jar;..\lib\mtj.jar ubic/erminej/classScoreGUI
 
