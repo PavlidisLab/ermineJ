f164aabc5a6b1100275e0e7e81a10bdb5072de7d
ppavlidis <paul@chibi.ubc.ca>
2013-05-20 00:14:26 +0000

    fix bugs in gradient bar

--- ermineJ-app/src/test/ubic/erminej/gui/GradientBarApp.java
+++ ermineJ-app/src/test/ubic/erminej/gui/GradientBarApp.java
@@ -1,3 +1,17 @@
+/*
+ * The ermineJ project
+ * 
+ * Copyright (c) 2013 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package ubic.erminej.gui;
 
 import java.awt.Color;
@@ -7,11 +21,15 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
 import ubic.basecode.graphics.ColorMap;
 import ubic.basecode.graphics.JGradientBar;
+import ubic.erminej.gui.util.GuiUtil;
 
 /**
+ * Test of gradient bar
+ * 
  * @author Will Braynen
  * @version $Id$
  */
@@ -37,11 +55,14 @@ public class GradientBarApp {
         frame.getContentPane().setLayout( new FlowLayout() );
         frame.setSize( new Dimension( 300, 300 ) );
         frame.setTitle( "JGradientBar Test" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
         Color[] colorMap = ColorMap.GREENRED_COLORMAP;
-        JGradientBar gradientBar = new JGradientBar();
+        JGradientBar gradientBar = new JGradientBar( colorMap );
         gradientBar.setColorMap( colorMap );
-        gradientBar.setLabels( -2, +2 );
+        gradientBar.setBorder( new EmptyBorder( 2, 2, 2, 2 ) );
+        // gradientBar.setBackground( Color.LIGHT_GRAY );
+        gradientBar.setLabels( -2.0, 2.0 );
 
         frame.getContentPane().add( gradientBar );
 
@@ -53,16 +74,8 @@ public class GradientBarApp {
         } else {
             frame.validate();
         }
-        // Center the window
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Dimension frameSize = frame.getSize();
-        if ( frameSize.height > screenSize.height ) {
-            frameSize.height = screenSize.height;
-        }
-        if ( frameSize.width > screenSize.width ) {
-            frameSize.width = screenSize.width;
-        }
-        frame.setLocation( ( screenSize.width - frameSize.width ) / 2, ( screenSize.height - frameSize.height ) / 2 );
+        GuiUtil.centerContainer( frame );
+
         frame.setVisible( true );
 
     }
